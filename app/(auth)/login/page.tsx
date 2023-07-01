d2f86466bfb1d466d89d54fe93bbd9a9b312a69f
Jeremy <jeremy.fran@icloud.com>
2023-07-01 20:57:25 +0800

    feat: add auth option picker
    Signed-off-by: jeremy <fran.jeremy@protonmail.com>

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NextPage } from "next";
+
+const LoginPage: NextPage = () => {
+  return (
+    <div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions">
+            <button className="btn-primary btn">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
