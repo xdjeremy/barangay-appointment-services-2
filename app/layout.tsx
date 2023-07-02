72fd348edc72369738a28080379acfcceeb30310
jeremy <jeremy.fran@icloud.com>
2023-07-02 10:44:32 +0800

    BREAKING: moved validations to each component folders
    Signed-off-by: jeremy <jeremy.fran@icloud.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Toaster />
       <body className={inter.className}>{children}</body>
     </html>
   );
