7980883abec538987c85ee815efe5faf5761c749
jeremy <jeremy.fran@icloud.com>
2023-08-05 21:33:58 +0800

    feat: add appointments page design
    Signed-off-by: jeremy <jeremy.fran@icloud.com>

---
+++ app/(dashboard)/appointments/components/appointments-header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AppointmentsHeader = () => {
+  return (
+    <div className={"mb-10 mt-16"}>
+      <div className="text-[64px] font-black text-gray-800">Appointment</div>
+      <div className="text-2xl font-extralight text-black">
+        Book an appointment
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentsHeader;
