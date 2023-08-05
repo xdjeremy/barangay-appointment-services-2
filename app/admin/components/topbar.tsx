0dd01ead296eaed206e3253150ad848fbc167473
jeremy <jeremy.fran@icloud.com>
2023-08-06 05:04:22 +0800

    refactor: remove icon on admin topbar
    Signed-off-by: jeremy <jeremy.fran@icloud.com>

--- app/admin/components/topbar.tsx
+++ app/admin/components/topbar.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { Bell, Mail, Menu } from "lucide-react";
 
 const AdminTopBar = () => {
   return (
     <div
       className={
-        "flex w-full flex-row items-center justify-between bg-zinc-100 p-6 text-black"
+        "flex w-full flex-row items-center justify-end bg-zinc-100 p-6 text-black"
       }
     >
-      <Menu />
       <div className={"flex flex-row items-center gap-5"}>
-        <Mail />
-        <Bell />
         <div className={"flex flex-col"}>
           <div className="text-xl font-semibold text-black">Ralph Anderson</div>
           <div className="text-xs font-light text-black">Admin</div>
