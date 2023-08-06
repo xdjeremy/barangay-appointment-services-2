8bfd9207b3f55d08f7386e4c7a7fb322cf3d5f2d
jeremy <jeremy.fran@icloud.com>
2023-08-06 14:48:29 +0800

    fix: news images
    Signed-off-by: jeremy <jeremy.fran@icloud.com>

--- app/(dashboard)/news/components/small-news-item.tsx
+++ app/(dashboard)/news/components/small-news-item.tsx
@@ -2,19 +2,18 @@ import React, { FC } from "react";
 import Image from "next/image";
 import { NewsResponse } from "@/types/pocketbase-types";
 import Link from "next/link";
+import { pocketbase } from "@/lib/utils/pocketbase";
 
 interface Props {
   news: NewsResponse;
 }
 
-9;
 const SmallNewsItem: FC<Props> = ({ news }) => {
+  const imageUrl = pocketbase.files.getUrl(news, news.image);
   return (
     <Link href={`/news/${news.id}`} className={"flex w-full flex-col"}>
       {news.image ? (
-        <div className={"w-full"}>
-          <Image src={news.image} alt={""} fill={true} />
-        </div>
+        <Image src={imageUrl} alt={""} height={209} width={400} />
       ) : (
         <div className={"h-[209px] w-full bg-white"}></div>
       )}
--- app/(dashboard)/news/page.tsx
+++ app/(dashboard)/news/page.tsx
@@ -17,43 +17,45 @@ const getNews = async (): Promise<NewsResponse[]> => {
 };
 
 const News = async () => {
-    const news = await getNews();
-    return (
-      <div>
-        <PageTitle title={"News"} subtitle={"Daily News & updates"} />
-        <div className={"flex flex-row items-start justify-between gap-10"}>
-          {/* big news */}
-          <div className={"flex w-full"}>
-            <BigNewsItem news={news[0]} />
-          </div>
-          {/*  big news end  */}
+  const news = await getNews();
 
-          {/*  small news  */}
-          <div className={"flex w-full flex-col items-center gap-4"}>
-            <SmallNewsItem news={news[1]} />
-            <SmallNewsItem news={news[2]} />
-          </div>
-          {/* small news end */}
+  return (
+    <div>
+      <PageTitle title={"News"} subtitle={"Daily News & updates"} />
+      <div className={"flex flex-row items-start justify-between gap-10"}>
+        {/* big news */}
+        <div className={"flex w-full"}>
+          <BigNewsItem news={news[0]} />
+        </div>
+        {/*  big news end  */}
 
-          {/*  updates  */}
-          <div className={"flex w-full flex-col"}>
-            <h2 className={"z-10 bg-base-300 text-[40px] font-bold text-black"}>
-              Updates
-            </h2>
-            <div
-              className={
-                "flex h-96 -translate-y-10 flex-col divide-y divide-solid divide-black overflow-auto"
-              }
-            >
-              {news.map((item) => {
+        {/*  small news  */}
+        <div className={"flex w-full flex-col items-center gap-4"}>
+          <SmallNewsItem news={news[1]} />
+          <SmallNewsItem news={news[2]} />
+        </div>
+        {/* small news end */}
+
+        {/*  updates  */}
+        <div className={"flex w-full flex-col"}>
+          <h2 className={"z-10 bg-base-300 text-[40px] font-bold text-black"}>
+            Updates
+          </h2>
+          <div
+            className={
+              "flex h-96 -translate-y-10 flex-col divide-y divide-solid divide-black overflow-auto"
+            }
+          >
+            {news &&
+              news.map((item) => {
                 return <UpdateItem key={item.id} text={item.title || ""} />;
               })}
-            </div>
           </div>
-          {/*  updated end  */}
         </div>
+        {/*  updated end  */}
       </div>
-    );
+    </div>
+  );
 };
 
 export default News;
